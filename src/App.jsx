007a4078a5860cad23ed1c56b54e31626150523a
Vasileios Milias <v.milias1@gmail.com>
2025-04-14 19:22:32 +0100

    Rebase develop and install prop-types

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import Hero from './components/Hero.jsx';
-import logo from './artwork/logo.jpg';
+import Hero from './components/Hero.jsx'
+import logo from './artwork/logo.jpg'
 import './index.css'
 
 function App() {
-
-
   return (
-      <>
-          <Hero
-              title="Daly Radiance Aesthetics"
-              description="Mental Health Nurse & Aesthetic Practitioner"
-              logo={logo}
-          />
-      </>
+    <>
+      <Hero
+        title="Daly Radiance Aesthetics"
+        description="Mental Health Nurse & Aesthetic Practitioner"
+        logo={logo}
+      />
+    </>
   )
 }
 
