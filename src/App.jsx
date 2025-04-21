34635d3dff492ade227139e98c4f6aeffdabd47b
Vasileios Milias <v.milias1@gmail.com>
2025-04-21 11:31:53 +0100

    Add propertis to hero element

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Hero from './components/Hero.jsx'
-
 import logo from './artwork/logo.jpg'
-
 import Services from './components/Services.jsx'
 
 import './index.css'
 
 function App() {
+
   return (
     <>
 
-      <Hero/>
+      <Hero title={"Shelley Aesthetics"} logo={logo}
+      description={"A modern aesthetic clinic specializing in Botox, dermal fillers, and non-surgical facial rejuvenation treatments to help clients look and feel their best."}/>
 
      
       <Services />
