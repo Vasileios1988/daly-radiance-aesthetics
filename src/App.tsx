a314acd3f680130793f6cdee190fd9de5ef8cef2
Vasileios Milias <v.milias1@gmail.com>
2025-04-23 20:35:12 +0100

    Create the form, input fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Hero from './components/Hero'
 import logo from './artwork/logo.jpg'
 import Services from './components/Services'
+import Contact from './components/Contact'
 
 import './index.css'
 
@@ -10,9 +11,9 @@ function App() {
 
       <Hero title={'Daly Radiance Aesthetics'} description={'Aesthetic treatments designed to refresh, not replace.'} logo={logo}/>
 
-     
       <Services />
 
+        <Contact />
     </>
   )
 }
