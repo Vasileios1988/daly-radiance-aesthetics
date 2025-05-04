be459956109201f4572902c5cebec5e5ce047252
Vasileios Milias <v.milias1@gmail.com>
2025-05-04 10:20:16 +0100

    Add contact

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import Hero from './components/Hero'
 import logo from './artwork/logo.jpg'
 import Services from './components/Services'
 import Contact from './components/Contact'
+
 import './index.css'
 
 function App() {
   return (
     <>
+        <Hero title={'Daly Radiance Aesthetics'} description={'Aesthetic treatments designed to refresh, not replace.'} logo={logo}/>
       <Hero
         title={'Daly Radiance Aesthetics'}
         description={'Aesthetic treatments designed to refresh, not replace.'}
@@ -19,7 +21,6 @@ function App() {
       <Services />
 
         <Contact />
-
     </>
   )
 }
