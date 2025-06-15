490fa1c6f8da6b90c0baf65ff423c38c9fa3bb60
Vasileios Milias <v.milias1@gmail.com>
2025-06-15 19:25:44 +0100

    Replace Shelley’s photo in About section

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import Hero from './components/Hero'
 import logo from './artwork/logo.jpg'
 import Services from './components/Services'
-import Contact from './components/Contact'
 import About from './components/About'
-import shelleyPhoto from './artwork/Shelley.png';
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
+import botox from './artwork/botox.jpg';
 
 import './index.css'
 
 function App() {
   return (
     <>
-      <Navbar />
       <Hero
         title={'Daly Radiance Aesthetics'}
         description={'Aesthetic treatments designed to refresh, not replace.'}
@@ -21,14 +17,7 @@ function App() {
 
       <Services />
 
-
-        <About photoUrl={shelleyPhoto} />
-
-
-      <Contact />
-
-      <Footer />
-
+        <About photoUrl={botox} />
     </>
   )
 }
